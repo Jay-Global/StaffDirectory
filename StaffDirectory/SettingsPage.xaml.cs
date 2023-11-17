9906916a1ca74a6282c2fe6c4004a8fa1c186041
Juuso Jay Koivunen <juuso.koivunen.personal@gmail.com>
2023-11-17 13:20:22 +1100

    Version 2.0
    Add settings, Bugs: minor responsive problems, settings not realtime

---
+++ StaffDirectory/SettingsPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿namespace StaffDirectory;
+
+public partial class SettingsPage : ContentPage
+{
+    private SettingsViewModel _viewModel;
+
+    public SettingsPage()
+    {
+        InitializeComponent();
+        _viewModel = new SettingsViewModel();
+        BindingContext = _viewModel;
+    }
+
+    private void OnThemeToggled(object sender, ToggledEventArgs e)
+    {
+        // Your logic to toggle the theme
+        Application.Current.UserAppTheme = e.Value ? AppTheme.Dark : AppTheme.Light;
+    }
+
+    // Other event handlers if necessary
+}
---
+++ StaffDirectory/SettingsViewModel.cs
@@ -0,0 +1,72 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+
+public class SettingsViewModel : INotifyPropertyChanged
+{
+    private bool _isDarkThemeEnabled;
+    public bool IsDarkThemeEnabled
+    {
+        get => _isDarkThemeEnabled;
+        set
+        {
+            if (_isDarkThemeEnabled != value)
+            {
+                _isDarkThemeEnabled = value;
+                OnPropertyChanged();
+                // Apply the theme
+                Application.Current.UserAppTheme = _isDarkThemeEnabled ? AppTheme.Dark : AppTheme.Light;
+            }
+        }
+    }
+
+    private double _fontSize = 12; // Default font size
+    public double FontSize
+    {
+        get => _fontSize;
+        set
+        {
+            if (_fontSize != value)
+            {
+                _fontSize = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+
+    private double _brightness = 1; // Default brightness
+    public double Brightness
+    {
+        get => _brightness;
+        set
+        {
+            if (_brightness != value)
+            {
+                _brightness = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+
+    private int _selectedFontIndex;
+    public int SelectedFontIndex
+    {
+        get => _selectedFontIndex;
+        set
+        {
+            if (_selectedFontIndex != value)
+            {
+                _selectedFontIndex = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+    protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
+
+
